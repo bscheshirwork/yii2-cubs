bb4ae5ac2d8bb520142f297219e2624e59372c88
bscheshirwork <bscheshir.work@gmail.com>
2018-03-15 16:49:40 +0300

    new syntax

--- src/generators/model/cubs/model.php
+++ src/generators/model/cubs/model.php
@@ -114,7 +114,7 @@ class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . 
      */
     public static function find()
     {
-        return new <?= $queryClassFullName ?>(get_called_class());
+        return new <?= $queryClassFullName ?>(static::class);
     }
 <?php endif; ?>
 }
